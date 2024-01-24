f277f47a39c575eb40d09db45b6c1e7b4effaf38
tumbersa <gleb.kapustin.95@gmail.com>
2024-01-24 17:49:17 +0300

    added profile

---
+++ Landmarks/Model/Profile.swift
@@ -0,0 +1,27 @@
+//
+//  Profile.swift
+//  Landmarks
+//
+//  Created by Глеб Капустин on 24.01.2024.
+//
+
+import Foundation
+
+struct Profile {
+    var username: String
+    var prefersNotifications = true
+    var seasonalPhoto = Season.winter
+    var goalDate = Date()
+    
+    static let `default` = Profile(username: "g_kumar")
+    
+    enum Season: String, CaseIterable, Identifiable {
+        case spring = "🌷"
+        case summer = "🌞"
+        case autumn = "🍂"
+        case winter = "☃️"
+        
+        var id: String { rawValue }
+        
+    }
+}
